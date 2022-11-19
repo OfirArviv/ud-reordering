c6ec6d4ee4fc6cf112952c8e9c70278ac56c821d
Ofir Arviv <ofirarviv@gmail.com>
2022-11-19 13:18:42 +0200

    script update

--- experiments/scripts/run_experiment_mtop_train.sh
+++ experiments/scripts/run_experiment_mtop_train.sh
@@ -46,7 +46,7 @@ do
        mkdir -p "$serialization_dir"
      fi
 
-     allennlp train "$PWD"/experiments/train_configs/copynet_transformer.jsonnet --serialization-dir "$serialization_dir" --include-package ud-reordering.experiments.allennlp_extensions --file-friendly-logging --overrides '{"pytorch_seed":'"$RANDOM"', "numpy_seed":'"$RANDOM"', "random_seed": '"$RANDOM"' }'
+     allennlp train "$PWD"/experiments/train_configs/copynet_transformer.jsonnet --serialization-dir "$serialization_dir" --include-package experiments --file-friendly-logging --overrides '{"pytorch_seed":'"$RANDOM"', "numpy_seed":'"$RANDOM"', "random_seed": '"$RANDOM"' }'
 
    done
 done
