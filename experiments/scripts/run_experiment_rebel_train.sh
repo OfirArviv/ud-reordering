3c8d646145008a28a755aef42b37cded59f3a654
Ofir Arviv <ofirarviv@gmail.com>
2022-11-27 19:45:41 +0200

    update rebel dataset script

--- experiments/scripts/run_experiment_rebel_train.sh
+++ experiments/scripts/run_experiment_rebel_train.sh
@@ -30,8 +30,8 @@ export pointer_vocab_size=300
 
 dataset_dir=experiments/processed_datasets/rebel/
 # Standard Order Model
-export train_data_path="$dataset_dir"/seq2seq_standard/english_train_50000.tsv
-export valid_data_path="$dataset_dir"/seq2seq_standard/english_dev_2000.tsv
+export train_data_path="$dataset_dir"/seq2seq_standard/english_train_144976.tsv
+export valid_data_path="$dataset_dir"/seq2seq_standard/english_dev_2001.tsv
 
 serialization_dir="$DIR"/english_standard/model_"$MODEL_IDX"/
 if [ ! -d "$serialization_dir" ]; then
@@ -50,8 +50,8 @@ do
    algo_arr=(HUJI RASOOLINI)
    for algo in "${algo_arr[@]}"
    do
-     export train_data_path="$dataset_dir"/$subdir/english_train_50000_reordered_by_"$lang"_"$algo".tsv
-     export test_data_path="$dataset_dir"/$subdir/english_dev_2000_reordered_by_"$lang"_"$algo".tsv
+     export train_data_path="$dataset_dir"/$subdir/english_train_144976_reordered_by_"$lang"_"$algo".tsv
+     export test_data_path="$dataset_dir"/$subdir/english_dev_2001_reordered_by_"$lang"_"$algo".tsv
 
      serialization_dir="$DIR"/english_reordered_by_"$lang"_"$algo"/model_"$MODEL_IDX"/
 
