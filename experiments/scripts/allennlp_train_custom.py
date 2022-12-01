b86272f35ba943809b20c9cfb5ad30978f3da43e
Ofir Arviv <ofirarviv@gmail.com>
2022-12-01 18:34:23 +0200

    temp

--- experiments/scripts/allennlp_train_custom.py
+++ experiments/scripts/allennlp_train_custom.py
@@ -4,31 +4,32 @@ import sys
 
 from allennlp.commands import main
 
-# See https://guide.allennlp.org/debugging#3
+def allennlp_train():
+    # See https://guide.allennlp.org/debugging#3
 
-config_file = "experiments/train_configs/relation_extraction_copynet.json"
+    config_file = "experiments/train_configs/relation_extraction_copynet.json"
 
-# Use overrides to train on CPU.
-overrides = json.dumps({"trainer": {"cuda_device": -1}})
+    # Use overrides to train on CPU.
+    overrides = json.dumps({"trainer": {"cuda_device": -1}})
 
-serialization_dir = "temp"
+    serialization_dir = "temp"
 
-# Training will fail if the serialization directory already
-# has stuff in it. If you are running the same training loop
-# over and over again for debugging purposes, it will.
-# Hence, we wipe it out in advance.
-# BE VERY CAREFUL NOT TO DO THIS FOR ACTUAL TRAINING!
-shutil.rmtree(serialization_dir, ignore_errors=True)
+    # Training will fail if the serialization directory already
+    # has stuff in it. If you are running the same training loop
+    # over and over again for debugging purposes, it will.
+    # Hence, we wipe it out in advance.
+    # BE VERY CAREFUL NOT TO DO THIS FOR ACTUAL TRAINING!
+    shutil.rmtree(serialization_dir, ignore_errors=True)
 
-# Assemble the command into sys.argv
-sys.argv = [
-    "allennlp",  # command name, not used by main
-    "train",
-    config_file,
-# "--recover",
-    "-s", serialization_dir,
-    "--include-package", "experiments.allennlp_extensions",
-    "-o", overrides,
-]
+    # Assemble the command into sys.argv
+    sys.argv = [
+        "allennlp",  # command name, not used by main
+        "train",
+        config_file,
+    # "--recover",
+        "-s", serialization_dir,
+        "--include-package", "experiments.allennlp_extensions",
+        "-o", overrides,
+    ]
 
-main()
+    main()
