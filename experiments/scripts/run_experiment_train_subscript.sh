9a104cdedbee04df4f9c746b800bcc7794882be3
Ofir Arviv <ofirarviv@gmail.com>
2022-12-16 22:16:08 +0200

    update

--- experiments/scripts/run_experiment_train_subscript.sh
+++ experiments/scripts/run_experiment_train_subscript.sh
@@ -14,15 +14,18 @@ export HOME
 best_model_file="$serialization_dir"/"metrics.json"
 if [ -f "$metrics_file" ]
 then
+  echo "exit"
   exit
 fi
 
 best_model_file="$serialization_dir"/"best.th"
 if [ -f "$best_model_file" ]
 then
-  allennlp train "$serialization_dir"/config.json --recover --serialization-dir "$serialization_dir" --include-package experiments --file-friendly-logging
+  echo "recover"
+  echo allennlp train "$serialization_dir"/config.json --recover --serialization-dir "$serialization_dir" --include-package experiments --file-friendly-logging
 else
-  allennlp train "$PWD"/experiments/train_configs/copynet_transformer.jsonnet --serialization-dir "$serialization_dir" --include-package experiments --file-friendly-logging --overrides '{"pytorch_seed":'"$RANDOM"', "numpy_seed":'"$RANDOM"', "random_seed": '"$RANDOM"' }'
+  echo "new"
+  echo allennlp train "$PWD"/experiments/train_configs/copynet_transformer.jsonnet --serialization-dir "$serialization_dir" --include-package experiments --file-friendly-logging --overrides '{"pytorch_seed":'"$RANDOM"', "numpy_seed":'"$RANDOM"', "random_seed": '"$RANDOM"' }'
 fi
 
 
