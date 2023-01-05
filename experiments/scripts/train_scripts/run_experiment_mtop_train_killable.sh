fe04c9f7ef89724790021c3329db48520bd8c4ca
Ofir Arviv <ofirarviv@gmail.com>
2023-01-06 00:58:42 +0200

    fix

--- experiments/scripts/train_scripts/run_experiment_mtop_train_killable.sh
+++ experiments/scripts/train_scripts/run_experiment_mtop_train_killable.sh
@@ -51,7 +51,7 @@ if [ "$KILLABLE" ]
     sbatch_params=""
 fi
 
-sbatch $sbatch_params experiments/scripts/run_experiment_train_subscript.sh
+sbatch $sbatch_params experiments/scripts/train_scripts/run_experiment_train_subscript.sh
 
 # Reordered Models
 combined_postfixes=("" "_combined")
