ae030549bdb33d0832d9d8b50e57fc565d81b08c
Ofir Arviv <ofirarviv@gmail.com>
2023-01-09 10:33:25 +0200

    smiler script update

---
+++ experiments/scripts/evaluate_scripts/smiler_eval.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+
+ARGPARSE_DESCRIPTION="Sample script description"      # this is optional
+source /cs/labs/oabend/ofir.arviv/argparse.bash || exit 1
+argparse "$@" <<EOF || exit 1
+parser.add_argument('-m', '--model-dir', required=True)
+parser.add_argument('-o', '--output-dir', required=True)
+parser.add_argument('-k', '--killable', action='store_true', default=False)
+
+EOF
+
+HOME="/cs/labs/oabend/ofir.arviv/"
+TMP=$TMPDIR
+TEMP=$TMPDIR
+export HOME
+
+export model_dir="$MODEL_DIR"/english_standard/
+export output_dir=$OUTPUT_DIR
+export test_dir="experiments/processed_datasets/smiler/standard/"
+
+if [ "$KILLABLE" ]
+ then
+   sbatch_params="--killable --requeue"
+  else
+    sbatch_params=""
+fi
+
+sbatch $sbatch_params -J eval_mtop experiments/scripts/evaluate_scripts/eval_subscript.sh
+
+# Reordered Models
+combined_postfixes=("" "_combined")
+languages=(arabic korean persian)
+algo_arr=(HUJI RASOOLINI)
+for combined_postfix in "${combined_postfixes[@]}"
+do
+  for lang in "${languages[@]}"
+  do
+    for algo in "${algo_arr[@]}"
+    do
+      export model_dir="$MODEL_DIR"/english_reordered_by_"$lang"_"$algo""$combined_postfix"/
+
+      sbatch $sbatch_params -J eval_mtop experiments/scripts/evaluate_scripts/eval_subscript.sh
+
+    done
+  done
+done
+
