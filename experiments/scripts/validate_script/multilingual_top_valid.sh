8a9617becd2be44e7ca47cd2356620d2252ea5de
Ofir Arviv <ofirarviv@gmail.com>
2023-01-09 17:31:53 +0200

    add validate scripts

---
+++ experiments/scripts/validate_script/multilingual_top_valid.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+ARGPARSE_DESCRIPTION="Sample script description"      # this is optional
+source /cs/labs/oabend/ofir.arviv/argparse.bash || exit 1
+argparse "$@" <<EOF || exit 1
+parser.add_argument('-m', '--model-dir', required=True)
+parser.add_argument('-c', '--count', required=True)
+
+EOF
+
+HOME="/cs/labs/oabend/ofir.arviv/"
+TMP=$TMPDIR
+TEMP=$TMPDIR
+export HOME
+
+. ../venv/bin/activate
+export PYTHONPATH=$PYTHONPATH:.
+
+export $expected_models_count="$COUNT"
+
+# Standard Order Model
+export model_dir="$DIR"/english_standard/
+
+python experiments/scripts/validate_script/validate_base.py -m "$model_dir" -c "$expected_models_count"
+
+# Reordered Models
+combined_postfixes=("" "_combined")
+languages=(japanese italian)
+algo_arr=(HUJI RASOOLINI HUJI_RASOOLINI)
+for combined_postfix in "${combined_postfixes[@]}"
+do
+  for lang in "${languages[@]}"
+  do
+    for algo in "${algo_arr[@]}"
+    do
+
+      export model_dir="$DIR"/english_reordered_by_"$lang"_"$algo""$combined_postfix"/
+
+      python experiments/scripts/validate_script/validate_base.py -m "$model_dir" -c "$expected_models_count"
+
+    done
+  done
+done
+
---
+++ experiments/scripts/validate_script/validate_base.py
@@ -0,0 +1,31 @@
+import argparse
+import glob
+import os.path
+
+
+def run_model_evaluation(main_models_dir: str, expected_models_count: int):
+    model = main_models_dir
+
+    assert os.path.isdir(model)
+    while model[-1] in ["/", "\\"]:
+        model = model[:-1]
+
+    existing_models = []
+    for model_idx_path in glob.glob(f'{model}/*'):
+        if not os.path.exists(f'{model_idx_path}/model.tar.gz'):
+            existing_models.append(model_idx_path)
+
+    existing_models_idx = [os.path.basename(p) for p in existing_models]
+    if set(existing_models_idx) != set(range(1, expected_models_count + 1)):
+        missing_models = set(range(1, expected_models_count + 1)).difference(set(existing_models_idx))
+        print(f'{main_models_dir} is missing the following models: {missing_models}')
+
+
+if __name__ == '__main__':
+    argparser = argparse.ArgumentParser(description="Evaluating method for Universal Dependencies")
+    argparser.add_argument("-m", "--model-dir", required=True)
+    argparser.add_argument("-c", "--expected-models-count", required=True)
+
+    args = argparser.parse_args()
+
+    run_model_evaluation(args.model_dir, args.expected_models_count)
