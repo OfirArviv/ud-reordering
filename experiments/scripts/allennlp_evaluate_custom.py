7268f1b65c6a4853f63a1d8f9c86fe902a14fa72
Ofir Arviv <ofirarviv@gmail.com>
2023-01-15 22:38:13 +0200

    revert

--- experiments/scripts/allennlp_evaluate_custom.py
+++ experiments/scripts/allennlp_evaluate_custom.py
@@ -17,8 +17,7 @@ def allennllp_evaluate(model_gz_path: str, dataset: str, output_file_path: str,
         "--file-friendly-logging",
         "--cuda-device", str(cuda_device),
         "--output-file", output_file_path,
-        "--include-package", "experiments.allennlp_extensions",
-        "--overrides", '{"model.decoder.type": "auto_regressive_seq_decoder_custom"}'
+        "--include-package", "experiments.allennlp_extensions"
     ]
     if predictions_output_file:
         sys.argv.extend([
