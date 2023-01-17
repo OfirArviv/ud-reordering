b35aa5ebc1c2e6592afd3424a6e75da58aefd0a9
Ofir Arviv <ofirarviv@gmail.com>
2023-01-17 17:12:09 +0200

    mt5 train

---
+++ experiments/scripts/train_scripts/mtop_mt5_train.sh
@@ -0,0 +1,77 @@
+#!/usr/bin/env bash
+
+ARGPARSE_DESCRIPTION="Sample script description"      # this is optional
+source /cs/labs/oabend/ofir.arviv/argparse.bash || exit 1
+argparse "$@" <<EOF || exit 1
+parser.add_argument('-d', '--dir', required=True)
+parser.add_argument('-i', '--experiment-num', required=False)
+parser.add_argument('-k', '--killable', action='store_true', default=False)
+
+EOF
+
+HOME="/cs/labs/oabend/ofir.arviv/"
+TMP=$TMPDIR
+TEMP=$TMPDIR
+export HOME
+
+
+MODEL_IDX="$EXPERIMENT_NUM"
+
+export vocab_path="experiments/vocabs/mtop_pointers"
+export metric_1="em_accuracy"
+export metric_2=null
+export validation_metric="+em_accuracy"
+export model_name="google/mt5-base"
+
+export config_file="mt5.jsonnet"
+
+
+dataset_dir=experiments/processed_datasets/mtop/non_pointer_format/
+# Standard Order Model
+export train_data_path="$dataset_dir"/standard/english_train_decoupled_format.tsv
+export valid_data_path=null
+export test_data_path="$dataset_dir"/standard/english_eval_decoupled_format.tsv
+
+export serialization_dir="$DIR"/english_standard/model_"$MODEL_IDX"/
+if [ ! -d "$serialization_dir" ]; then
+ echo "$serialization_dir" does not exists. Creating...
+ mkdir -p "$serialization_dir"
+fi
+
+if [ "$KILLABLE" ]
+ then
+   sbatch_params="--killable --requeue"
+  else
+    sbatch_params=""
+fi
+
+sbatch $sbatch_params -J train_top experiments/scripts/train_scripts/train_subscript.sh
+
+# Reordered Models
+combined_postfixes=("" "_combined")
+languages=(hindi)
+algo_arr=(HUJI RASOOLINI)
+for combined_postfix in "${combined_postfixes[@]}"
+do
+  for lang in "${languages[@]}"
+  do
+    subdir=english_reordered_by_"$lang"
+    for algo in "${algo_arr[@]}"
+    do
+      export train_data_path="$dataset_dir"/"$subdir"/english_train_decoupled_format_reordered_by_"$lang"_"$algo""$combined_postfix".tsv
+      export valid_data_path=null
+      export test_data_path="$dataset_dir"/"$subdir"/english_eval_decoupled_format_reordered_by_"$lang"_"$algo""$combined_postfix".tsv
+
+      export serialization_dir="$DIR"/english_reordered_by_"$lang"_"$algo""$combined_postfix"/model_"$MODEL_IDX"/
+
+      if [ ! -d "$serialization_dir" ]; then
+        echo "$serialization_dir" does not exists. Creating...
+        mkdir -p "$serialization_dir"
+      fi
+
+      sbatch $sbatch_params -J train_top experiments/scripts/train_scripts/train_subscript.sh
+
+    done
+  done
+done
+
---
+++ experiments/train_configs/mt5.jsonnet
@@ -0,0 +1,71 @@
+// =================== Configurable Settings ======================
+local train_data_path = std.extVar('train_data_path');
+local valid_data_path = std.extVar('valid_data_path');
+local test_data_path = std.extVar('test_data_path');
+local metric_1 = std.extVar('metric_1');
+local metric_2 = std.extVar('metric_2');
+local validation_metric = std.extVar('validation_metric');
+local model_name = std.extVar('model_name');
+// ================================================================
+{
+  "dataset_reader": {
+    "type": "seq2seq_length_filtering",
+    "source_max_tokens": 100,
+    "target_max_tokens": 200,
+    "source_add_start_token": false,
+    "source_add_end_token": false,
+    "source_tokenizer": {
+          "type": "pretrained_transformer",
+          "model_name": model_name
+        },
+    "target_tokenizer": {
+       "type": "whitespace"
+    },
+    "source_token_indexers": {
+      "tokens": {
+        "type": "pretrained_transformer",
+        "model_name": model_name,
+        "max_length": 1024
+      }
+    },
+    "target_token_indexers": {
+      "target_tokens": {
+        "type": "single_id",
+        "namespace": "target_tokens"
+      }
+    }
+  },
+  "train_data_path": train_data_path,
+  [if valid_data_path != "null" then "validation_data_path" else null]: valid_data_path,
+  [if test_data_path != "null" then "test_data_path" else null]: test_data_path,
+  [if test_data_path != "null" then "evaluate_on_test" else null]: true,
+  "model": {
+    "type": "mt5",
+    "model_name": "google/mt5-base",
+    "beam_size": 4,
+    "max_steps": 100,
+    [if metric_2 == "null" then "token_based_metric" else null]: [metric_1],
+    [if metric_2 != "null" then "token_based_metric" else null]: [metric_1, metric_2],
+  },
+  "data_loader": {
+    "batch_sampler": {
+      "type": "bucket",
+      "padding_noise": 0.0,
+      "batch_size":16
+    }
+  },
+  "trainer": {
+    "num_epochs": 50,
+    "patience": 10,
+    "grad_norm": 5.0,
+    "validation_metric": validation_metric,
+    "optimizer": {
+      "type": "huggingface_adamw",
+      "lr": 1e-5,
+      "weight_decay": 0.01,
+    },
+    "checkpointer": {
+      "keep_most_recent_by_count": 1
+    }
+  }
+}
