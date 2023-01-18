e8cb743546cffe644bb3800743003d72c6de9848
Ofir Arviv <ofirarviv@gmail.com>
2023-01-18 22:12:31 +0200

    agg_script

---
+++ experiments/scripts/agg_scripts/agg.py
@@ -0,0 +1,131 @@
+import argparse
+import glob
+import json
+import os.path
+import statistics
+from collections import defaultdict
+import pandas as pd
+
+from experiments.scripts.allennlp_predict_custom import allennllp_predict
+
+
+def get_lang_from_filename(filename: str):
+    if filename.startswith("en"):
+        lang = "english"
+    elif "english" in filename:
+        lang = "english"
+    elif filename.startswith("ar"):
+        lang = "arabic"
+    elif filename.startswith("de"):
+        lang = "german"
+    elif filename.startswith("es"):
+        lang = "spanish"
+    elif filename.startswith("fa"):
+        lang = "persian"
+    elif filename.startswith("fr"):
+        lang = "french"
+    elif filename.startswith("it"):
+        lang = "italian"
+    elif filename.startswith("ko"):
+        lang = "korean"
+    elif filename.startswith("nl"):
+        lang = "dutch"
+    elif filename.startswith("pl"):
+        lang = "polish"
+    elif filename.startswith("pt"):
+        lang = "portuguese"
+    elif filename.startswith("ru"):
+        lang = "russian"
+    elif filename.startswith("hi"):
+        lang = "hindi"
+    elif filename.startswith("th"):
+        lang = "thai"
+    elif filename.startswith("ja"):
+        lang = "japanese"
+    elif filename.startswith("tr"):
+        lang = "turkish"
+    elif filename.startswith("sv"):
+        lang = "swedish"
+    elif filename.startswith("fa"):
+        lang = "persian"
+    elif filename.startswith("id"):
+        lang = "indonesian"
+    elif filename.startswith("ga"):
+        lang = "irish"
+    else:
+        raise Exception(f'Unknown lang: {filename}')
+
+    return lang
+
+
+def run_agg_evaluation(main_models_dir: str, output_dir: str):
+    from experiments.scripts.allennlp_evaluate_custom import allennllp_evaluate
+
+    # Dict of metric -> dataframe
+    val_dict = defaultdict(pd.DataFrame)
+    model_type = glob.glob(f'{main_models_dir}/*/')
+
+    for model in model_type:
+        model_basename = os.path.basename(model.strip("\\").strip("/"))
+
+        if "reordered" in model_basename:
+            model_lang = model_basename.split("english_reordered_by_")[1].split("_")[0]
+        else:
+            model_lang = "english"
+
+        if "HUJI_combined" in model_basename:
+            model_type = "HUJI_ENSEMBLE"
+        elif "HUJI" in model_basename:
+            model_type = "HUJI"
+        elif "RASOOLINI_combined" in model_basename:
+            model_type = "RASOOLINI_ENSEMBLE"
+        elif "RASOOLINI" in model_basename:
+            model_type = "RASOOLINI"
+        elif "standard" in model_basename:
+            model_type = "VANILLA"
+        else:
+            raise NotImplementedError(model_basename)
+
+
+        datasets_dirs = glob.glob(f'{model}/*/')
+        datasets_dirs = [d for d in datasets_dirs if d != "predictions"]
+
+        for dataset in datasets_dirs:
+            dataset_name = os.path.basename(dataset.strip("\\").strip("/"))
+
+            dataset_lang = get_lang_from_filename(dataset_name)
+
+            if model_lang != "english" and model_lang != dataset_lang:
+                print(f'model lang {model_lang} does not match dataset lang {dataset_lang}')
+                print(f'model name: {model_basename}')
+                print(f'dataset name: {dataset_name}')
+                continue
+
+            agg_file = glob.glob(f'{dataset}/*agg*.json')
+            assert len(agg_file) == 1
+            agg_file = agg_file[0]
+
+            with open(agg_file, 'r', encoding='utf-8') as f:
+                agg_data = json.load(f)
+
+            for metric_k, v in agg_data.items():
+                if metric_k == "model_count":
+                    continue
+                df = val_dict[metric_k]
+                df.loc[dataset_lang, model_type] = v
+
+    for metric, df in val_dict.items():
+        output_path = f'{output_dir}/{metric}.csv'
+        os.makedirs(output_dir, exist_ok=True)
+        df.to_csv(output_path)
+
+
+if __name__ == '__main__':
+    run_agg_evaluation('experiments_results/evaluation/mtop', "a")
+    argparser = argparse.ArgumentParser(description="Evaluating method for Universal Dependencies")
+    argparser.add_argument("-m", "--model-dir", required=True)
+    argparser.add_argument("-o", "--output-dir", required=True)
+
+    args = argparser.parse_args()
+
+    run_agg_evaluation(args.model_dir, args.output_dir)
