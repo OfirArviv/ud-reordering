51780daa26269a979f948ab506671a86c293fdde
Ofir Arviv <ofirarviv@gmail.com>
2023-01-20 18:40:10 +0200

    script updates

--- experiments/scripts/evaluate_scripts/eval_subscript.sh
+++ experiments/scripts/evaluate_scripts/eval_subscript.sh
@@ -15,7 +15,7 @@ export PYTHONPATH=$PYTHONPATH:.
 
 if [ -z "$eval_all" ]; then extra_args=""; else extra_args="--eval-on-all"; fi
 
-echo python experiments/scripts/evaluate_scripts/evaluate_base.py -m "$model_dir" -o "$output_dir" -t "$test_dir" extra_args
+echo python experiments/scripts/evaluate_scripts/evaluate_base.py -m "$model_dir" -o "$output_dir" -t "$test_dir" $extra_args
 
 
 
