c2577f1609cd9e343a0c64cb86d42d6e030d4a5f
Ofir Arviv <ofirarviv@gmail.com>
2023-06-10 16:55:43 +0300

    finetune scripts

--- experiments/scripts/train_scripts/finetune_scripts/mtop_mt5_finetune.sh
+++ experiments/scripts/train_scripts/finetune_scripts/mtop_mt5_finetune.sh
@@ -40,7 +40,6 @@ do
   export model_archive="$DIR"/english_standard/model_"$MODEL_IDX"/
   for examples_count in "${count_arr[@]}"
   do
-    echo "$examples_count"
     export examples_count="$examples_count"
     export serialization_dir="$DIR"/finetuned/english_standard_finetuned_"$lang"_"$examples_count"/model_"$MODEL_IDX"/
     if [ ! -d "$serialization_dir" ]; then
@@ -70,7 +69,6 @@ do
       for examples_count in "${count_arr[@]}"
       do
         export examples_count="$examples_count"
-        echo "$examples_count"
         export serialization_dir="$DIR"/finetuned/english_reordered_by_"$lang"_"$algo""$combined_postfix"_finetuned_"$examples_count"/model_"$MODEL_IDX"/
         if [ ! -d "$serialization_dir" ]; then
           echo "$serialization_dir" does not exists. Creating...
