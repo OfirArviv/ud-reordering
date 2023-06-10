ffb2c362ba604930c3e1a027f9a092c9695fb8b7
Ofir Arviv <ofirarviv@gmail.com>
2023-06-10 16:41:01 +0300

    finetune scripts

--- experiments/scripts/train_scripts/finetune_scripts/mtop_mt5_finetune.sh
+++ experiments/scripts/train_scripts/finetune_scripts/mtop_mt5_finetune.sh
@@ -31,14 +31,14 @@ dataset_dir=experiments/processed_datasets/mtop/non_pointer_format/
 
 languages=(hindi)
 algo_arr=(HUJI RASOOLINI)
-count_arr=( "100" "200" "300" )
+count_arr=( 100 200 300 )
 for lang in "${languages[@]}"
 do
   export train_data_path="$dataset_dir"/standard_small/"$lang"_eval_decoupled_format.tsv
   export valid_data_path=null
   export test_data_path="$dataset_dir"/standard/"$lang"_test_decoupled_format.tsv
   export model_archive="$DIR"/english_standard/model_"$MODEL_IDX"/
-  for examples_count in "${!count_array[@]}"
+  for examples_count in "${count_array[@]}"
   do
     export examples_count="$examples_count"
     export serialization_dir="$DIR"/finetuned/english_standard_finetuned_"$lang"_"$examples_count"/model_"$MODEL_IDX"/
@@ -67,7 +67,7 @@ do
     do
       export model_archive="$DIR"/english_reordered_by_"$lang"_"$algo""$combined_postfix"/model_"$MODEL_IDX"/
 
-      for examples_count in "${!count_array[@]}"
+      for examples_count in "${count_array[@]}"
       do
         export examples_count="$examples_count"
         export serialization_dir="$DIR"/finetuned/english_reordered_by_"$lang"_"$algo""$combined_postfix"_finetuned_"$examples_count"/model_"$MODEL_IDX"/
