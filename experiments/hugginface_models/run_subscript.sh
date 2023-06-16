aad17ef066c2d65c5fe6343226a42c77e4888ce7
Ofir Arviv <ofirarviv@gmail.com>
2023-06-16 20:03:08 +0300

    scripts update

---
+++ experiments/hugginface_models/run_subscript.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+#SBATCH --mem=32G
+#SBATCH --time=2-0
+#SBATCH --gres=gpu:1,vmem:20g
+#SBATCH -c2
+
+HOME="/cs/labs/oabend/ofir.arviv/"
+TMP=$TMPDIR
+TEMP=$TMPDIR
+export HOME
+
+. ../venv_hf/bin/activate
+
+module load cuda/11.7
+export PYTHONPATH=.:$PYTHONPATH
+python "$PWD"/experiments/hugginface_models/train_model.py
+
+
