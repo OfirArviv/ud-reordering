6a12b481571cc2a5143820bb738e65d3fe3cae79
Ofir Arviv <ofirarviv@gmail.com>
2023-06-16 11:12:22 +0300

    scripts update

--- experiments/scripts/evaluate_scripts/finetune_evaluation/tacred_eval.sh
+++ experiments/scripts/evaluate_scripts/finetune_evaluation/tacred_eval.sh
@@ -3,7 +3,7 @@
 ARGPARSE_DESCRIPTION="Sample script description"      # this is optional
 source /cs/labs/oabend/ofir.arviv/argparse.bash || exit 1
 argparse "$@" <<EOF || exit 1
-parser.add_argument('-m', '--model-dir', required=True)
+parser.add_argument('-m', '--dir', required=True)
 parser.add_argument('-o', '--output-dir', required=True)
 parser.add_argument('-k', '--killable', action='store_true', default=False)
 
