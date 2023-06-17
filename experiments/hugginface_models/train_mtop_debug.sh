7f6a51774dce4668d9ed804953db43802ba7075d
Ofir Arviv <ofirarviv@gmail.com>
2023-06-17 23:03:43 +0300

    script update

--- experiments/hugginface_models/train_mtop_debug.sh
+++ experiments/hugginface_models/train_mtop_debug.sh
@@ -10,8 +10,8 @@ export model_id="facebook/xglm-7.5B"
 export train_dataset_path="experiments/processed_datasets/mtop/non_pointer_format/standard/english_train_decoupled_format.tsv"
 export dev_dataset_path="experiments/processed_datasets/mtop/non_pointer_format/standard/english_eval_decoupled_format.tsv"
 export output_dir="temp_output_xglm_mtop"
-export use_lora=true
-export use_qlora=true
+export use_lora=1
+export use_qlora=1
 
 
 if [ ! -d "$output_dir" ]; then
