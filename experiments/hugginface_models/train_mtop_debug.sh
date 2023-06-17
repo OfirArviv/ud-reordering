a34abb03e219c9a2e65cbc8b9c3c0e975064c2ed
Ofir Arviv <ofirarviv@gmail.com>
2023-06-17 23:53:41 +0300

    xnli datasets

--- experiments/hugginface_models/train_mtop_debug.sh
+++ experiments/hugginface_models/train_mtop_debug.sh
@@ -19,5 +19,4 @@ if [ ! -d "$output_dir" ]; then
  mkdir -p "$output_dir"
 fi
 
-#sbatch -J hf_mtop experiments/hugginface_models/run_subscript.sh
-bash experiments/hugginface_models/run_subscript.sh
+sbatch -J hf_mtop experiments/hugginface_models/run_subscript.sh
