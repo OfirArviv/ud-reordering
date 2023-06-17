0488ffbae4804248b7851b01c5a6b41836166ffe
Ofir Arviv <ofirarviv@gmail.com>
2023-06-18 00:29:35 +0300

    xnli datasets

--- experiments/hugginface_models/train_xnli_tryout.sh
+++ experiments/hugginface_models/train_xnli_tryout.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
+ARGPARSE_DESCRIPTION="Sample script description"      # this is optional
+source /cs/labs/oabend/ofir.arviv/argparse.bash || exit 1
+argparse "$@" <<EOF || exit 1
+parser.add_argument('-i', '--id', required=True)
+parser.add_argument('-k', '--killable', action='store_true', default=False)
+
+EOF
+
 HOME="/cs/labs/oabend/ofir.arviv/"
 TMP=$TMPDIR
 TEMP=$TMPDIR
@@ -9,7 +17,21 @@ export action="train"
 export model_id="google/mt5-base"
 export train_dataset_path="experiments/processed_datasets/xnli/standard/english_xnli_train.csv"
 export dev_dataset_path="experiments/processed_datasets/xnli/standard/english_xnli_eval.csv"
-export output_dir="temp_outputs/mt5_xnli/en_standard_1"
+export output_dir="temp_outputs/mt5_xnli/en_standard_""$ID"
+# export use_lora=null
+# export use_qlora=null
+
+
+if [ ! -d "$output_dir" ]; then
+ echo "$output_dir" does not exists. Creating...
+ mkdir -p "$output_dir"
+fi
+
+sbatch -J hf_xnli experiments/hugginface_models/run_subscript.sh
+
+export train_dataset_path="experiments/processed_datasets/xnli/english_reordered_by_hindi/english_xnli_train_reordered_by_hindi_HUJI.csv"
+export dev_dataset_path="experiments/processed_datasets/xnli/english_reordered_by_hindi/english_xnli_eval_reordered_by_hindi_HUJI.csv"
+export output_dir="temp_outputs/mt5_xnli/en_reordered_by_hindi_""$ID"
 # export use_lora=null
 # export use_qlora=null
 
