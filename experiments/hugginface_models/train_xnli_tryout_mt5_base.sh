49d50345d09fe232a066208d71c1c5ad5a2d583b
Ofir Arviv <ofirarviv@gmail.com>
2023-06-18 17:13:10 +0300

    dataset add

--- experiments/hugginface_models/train_xnli_tryout_mt5_base.sh
+++ experiments/hugginface_models/train_xnli_tryout_mt5_base.sh
@@ -18,7 +18,7 @@ export model_id="google/mt5-base"
 
 
 export train_dataset_path="experiments/processed_datasets/xnli/english_reordered_by_thai/english_xnli_train_reordered_by_thai_HUJI.csv"
-# export dev_dataset_path="experiments/processed_datasets/xnli/english_reordered_by_thai/english_xnli_eval_reordered_by_thai_HUJI.csv"
+export dev_dataset_path="experiments/processed_datasets/xnli/english_reordered_by_thai/english_xnli_eval_reordered_by_thai_HUJI.csv"
 export output_dir="temp_outputs/mt5_base_xnli/en_reordered_by_thai_""$ID"
 # export use_lora=null
 # export use_qlora=null
