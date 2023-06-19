a01a8b1ccdd38b4f1f9fe61ede2e13aa5792ffa6
Ofir Arviv <ofirarviv@gmail.com>
2023-06-19 23:44:28 +0300

    finalize hf code

--- experiments/hugginface_models/eval_xnli_tryout_mt5_base.sh
+++ experiments/hugginface_models/eval_xnli_tryout_mt5_base.sh
@@ -33,4 +33,4 @@ sbatch --killable --requeue -J eval_xnli experiments/hugginface_models/eval_subs
 
 export model_id="temp_outputs/mt5_base_xnli/en_reordered_by_thai_""$ID"
 export output_dir="temp_outputs/mt5_base_xnli/en_reordered_by_thai_""$ID"
-sbatch --killable --requeue -J eval_xnli experiments/hugginface_models/eval_subscript.sh
+# sbatch --killable --requeue -J eval_xnli experiments/hugginface_models/eval_subscript.sh
