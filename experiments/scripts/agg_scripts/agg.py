964cf21dc0137cf197cd4212fdd99fa5d7509a1e
Ofir Arviv <ofirarviv@gmail.com>
2023-06-19 16:25:58 +0300

    mtop dataset update

--- experiments/scripts/agg_scripts/agg.py
+++ experiments/scripts/agg_scripts/agg.py
@@ -55,6 +55,8 @@ def get_lang_from_filename(filename: str):
         return "telugu"
     elif "bangali" in filename:
         return "bengali"
+    elif "spanish" in filename:
+        return "spanish"
     elif filename.startswith("_"):
         return "_"
     else:
