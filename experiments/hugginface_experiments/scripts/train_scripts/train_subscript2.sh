771750372a328cc447e741c141c038f17d893984
Ofir Arviv <ofirarviv@gmail.com>
2023-06-20 15:37:32 +0300

    finalize hf code

---
+++ experiments/hugginface_experiments/scripts/train_scripts/train_subscript2.sh
@@ -0,0 +1,55 @@
+#!/usr/bin/env bash
+#SBATCH --mem=124g
+#SBATCH --time=7-0
+#SBATCH --gres=gpu:1,vmem:20g
+#SBATCH -c2
+
+HOME="/cs/labs/oabend/ofir.arviv/"
+TMP=$TMPDIR
+TEMP=$TMPDIR
+export HOME
+
+. ../venv_hf/bin/activate
+
+module load cuda/11.7
+export PYTHONPATH=.:$PYTHONPATH
+
+extra_params=""
+if [ "$use_lora" ]
+ then
+   extra_params="$extra_params"" --lora"
+fi
+
+if [ "$use_qlora" ]
+ then
+   extra_params="$extra_params"" --qlora"
+fi
+
+if [ "$add_instruct" ]
+ then
+   extra_params="$extra_params"" --add-instruction"
+fi
+
+if [ "$max_length" ]
+ then
+   extra_params="$extra_params"" --max-length"" $max_length"
+fi
+
+if [ "$eval_dataset_path" ]
+ then
+   extra_params="$extra_params"" --eval-dataset-path ""$eval_dataset_path"
+fi
+
+if [ "$test_dataset_path" ]
+ then
+   extra_params="$extra_params"" --test-dataset-path ""$test_dataset_path"
+fi
+
+if [ "$train_8_bits" ]
+ then
+   extra_params="$extra_params"" --train-8-bits "
+fi
+
+python "$PWD"/experiments/hugginface_experiments/src/run_2.py train --model-id  "$model_id" --train-dataset-path "$train_dataset_path"  --output-dir "$output_dir" --seed "$RANDOM" $extra_params
+
+
---
+++ experiments/hugginface_experiments/scripts/train_scripts/train_xglm_7_5_mtop_8_bits_fixed.sh
@@ -0,0 +1,74 @@
+#!/usr/bin/env bash
+
+ARGPARSE_DESCRIPTION="Sample script description"      # this is optional
+source /cs/labs/oabend/ofir.arviv/argparse.bash || exit 1
+argparse "$@" <<EOF || exit 1
+parser.add_argument('-d', '--dir', required=True)
+parser.add_argument('-i', '--model-idx', required=False)
+parser.add_argument('-k', '--killable', action='store_true', default=False)
+
+EOF
+
+HOME="/cs/labs/oabend/ofir.arviv/"
+TMP=$TMPDIR
+TEMP=$TMPDIR
+export HOME
+
+if [ "$KILLABLE" ]
+ then
+   sbatch_params="--killable --requeue"
+  else
+    sbatch_params=""
+fi
+
+export model_id="facebook/xglm-7.5B"
+export use_lora=1
+export train_8_bits=1
+export add_instruct=1
+export max_length=200
+
+dataset_dir="experiments/processed_datasets/mtop/non_pointer_format"
+# Vanilla model
+export train_dataset_path="$dataset_dir""/standard/english_train_decoupled_format.tsv"
+export eval_dataset_path="$dataset_dir""/standard/english_eval_decoupled_format.tsv"
+export test_dataset_path=$eval_dataset_path
+
+export output_dir="$DIR"/english_standard/model_"$MODEL_IDX"/
+
+if [ ! -d "$output_dir" ]; then
+ echo "$output_dir" does not exists. Creating...
+ mkdir -p "$output_dir"
+fi
+
+sbatch $sbatch_params -J xglm_mtop experiments/hugginface_experiments/scripts/train_scripts/train_subscript.sh
+
+exit 0
+# Reordered Models
+combined_postfixes=("_combined")
+# hindi thai french spanish german japanese tamil turkish
+languages=(hindi thai tamil)
+algo_arr=( HUJI )
+for combined_postfix in "${combined_postfixes[@]}"
+do
+  for lang in "${languages[@]}"
+  do
+    subdir=english_reordered_by_"$lang"
+    for algo in "${algo_arr[@]}"
+    do
+      export train_dataset_path="$dataset_dir"/"$subdir"/english_train_decoupled_format_reordered_by_"$lang"_"$algo""$combined_postfix".tsv
+      export eval_dataset_path="$dataset_dir"/"$subdir"/english_eval_decoupled_format_reordered_by_"$lang"_"$algo""$combined_postfix".tsv
+      export test_dataset_path=$eval_dataset_path
+
+      export output_dir="$DIR"/english_reordered_by_"$lang"_"$algo""$combined_postfix"/model_"$MODEL_IDX"/
+
+      if [ ! -d "$output_dir" ]; then
+        echo "$output_dir" does not exists. Creating...
+        mkdir -p "$output_dir"
+      fi
+
+      sbatch $sbatch_params -J xglm_mtop experiments/hugginface_experiments/scripts/train_scripts/train_subscript2.sh
+
+    done
+  done
+done
+
