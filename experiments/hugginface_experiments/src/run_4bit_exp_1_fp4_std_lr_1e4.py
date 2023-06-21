dbb06b3f1fc65783df22f0a735d28baca10c4f77
Ofir Arviv <ofirarviv@gmail.com>
2023-06-21 14:05:55 +0300

    finalize hf code

--- experiments/hugginface_experiments/src/run_4bit_exp_1_fp4_std_lr_1e4.py
+++ experiments/hugginface_experiments/src/run_4bit_exp_1_fp4_std_lr_1e4.py
@@ -377,9 +377,9 @@ def train_model(model_id: str,
         "quantization_config": bnb_config,
         "cache_dir": cache_dir,
         "trust_remote_code": True,
-        "torch_dtype": torch.bfloat16 if (train_in_4_bit or train_in_8_bit) else None,
-        "load_in_8bit": train_in_8_bit,
-        "device_map": "auto"
+        # "torch_dtype": torch.bfloat16 if (train_in_4_bit or train_in_8_bit) else None,
+        # "load_in_8bit": train_in_8_bit,
+        # "device_map": "auto"
     }
 
     if is_seq2seq_model:
@@ -422,9 +422,10 @@ def train_model(model_id: str,
         per_device_train_batch_size= 1 if train_in_8_bit else 4,  # if "base" in model_id else 1,
         per_device_eval_batch_size= 1 if train_in_8_bit else 4,  # if  "base" in model_id else 1,
 
-        logging_strategy='epoch',
-        evaluation_strategy='epoch' if eval_dataset else "no",
-        save_strategy='epoch',
+        logging_strategy='steps',
+        evaluation_strategy='steps' if eval_dataset else "no",
+        save_strategy='steps',
+        logging_steps=500,
         save_total_limit=2,
         load_best_model_at_end=eval_dataset is not None,
         metric_for_best_model="exact_match" if eval_dataset else None,
