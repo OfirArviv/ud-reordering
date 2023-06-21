70d3956af2b6ab6f2720ac7dee5928c74038f51d
Ofir Arviv <ofirarviv@gmail.com>
2023-06-21 12:40:37 +0300

    finalize hf code

--- experiments/hugginface_experiments/src/run_llama.py
+++ experiments/hugginface_experiments/src/run_llama.py
@@ -770,7 +770,8 @@ if __name__ == '__main__':
                          "facebook/xglm-564M",
                          "bigscience/bloom-7b1",
                          "facebook/xglm-564M"
-                         "tiiuae/falcon-7b-instruct"]
+                         "tiiuae/falcon-7b-instruct",
+                         "openlm-research/open_llama_13b"]
     model_list_seq2seq = ["google/flan-t5-xxl",
                           "google/mt5-base",
                           "google/flan-ul2"]
